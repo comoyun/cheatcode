83dd43d904a2615d0ec873189ed27a6257b6c82e
Khumoyun <contact@xumoyun.uz>
2025-04-28 00:22:15 +0500

    solve: 0001 (js)

---
+++ solutions/0001-two-sum/01.js
@@ -0,0 +1,23 @@
+/*
+ * @title Hashmap
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const search = target - num;
+        if (map.has(search)) return [map.get(search), i];
+        map.set(num, i);
+    }
+
+    /* No need to return anything here,
+     * as the problem guarantees at least one solution. */
+};
