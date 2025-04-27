ee5cdab6095688ea132067a3034fc636538f0679
Khumoyun <contact@xumoyun.uz>
2025-04-28 00:29:19 +0500

    solve: 0001 (js)

---
+++ solutions/0001-two-sum/02.js
@@ -0,0 +1,19 @@
+/*
+ * @title Brute Force
+ * @time: O(n^2)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let result = nums[i] + nums[j];
+            if (result === target) return [i, j];
+        }
+    }
+};
