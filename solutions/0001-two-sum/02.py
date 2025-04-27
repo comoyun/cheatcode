39ef04ff21b249cf362786b2a75cd6599bfa82bc
Khumoyun <contact@xumoyun.uz>
2025-04-28 00:32:59 +0500

    solve: 0001 (py)

---
+++ solutions/0001-two-sum/02.py
@@ -0,0 +1,17 @@
+# 
+# @title Brute Force
+# @time: O(n^2)
+# @space: O(1)
+#
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            result = nums[i] + nums[j]
+            if result == target:
+                return [i, j]
