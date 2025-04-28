0f0825deb59ca3ee5e4e9d3747007d8a70cf5c63
Khumoyun <contact@xumoyun.uz>
2025-04-28 09:52:02 +0500

    solve: 2302 (js)

---
+++ solutions/2302-count-subarrays-with-score-less-than-k/02.js
@@ -0,0 +1,33 @@
+/*
+ * @title Sliding Window
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countSubarrays = (nums, k) => {
+    let left = 0,
+        right = 0,
+        sum = 0,
+        length = 0,
+        count = 0;
+
+    while (right < nums.length) {
+        sum += nums[right];
+        length = right - left + 1;
+
+        while (sum * length >= k && left <= right) {
+            sum -= nums[left++];
+            length = right - left + 1;
+        }
+
+        count += length;
+        right++;
+    }
+
+    return count;
+};
