d1d350ae1c93e2f634181a208d328d3314975191
Khumoyun <contact@xumoyun.uz>
2025-04-28 19:56:25 +0500

    solve: 0001 (py)

---
+++ solutions/0001-two-sum/03.py
@@ -0,0 +1,25 @@
+#
+# @title Two Pointers
+# @time: O(n log n)
+# @space: O(n)
+#
+
+def twoSum(nums, target):
+    """
+    nums: list[int]
+    target: int
+    return: list[int]
+    """
+    nums = [[i, num] for i, num in enumerate(nums)]
+    nums.sort(key=lambda x: x[1])
+
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        sum_ = nums[left][1] + nums[right][1]
+        if sum_ == target:
+            return [nums[left][0], nums[right][0]]
+        if sum_ > target:
+            right -= 1
+        else:
+            left += 1
