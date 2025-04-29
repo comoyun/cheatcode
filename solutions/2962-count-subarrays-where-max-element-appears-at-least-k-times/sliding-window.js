7618be89411a9abadde9384aab18612cfc91d3fc
Khumoyun <contact@xumoyun.uz>
2025-04-29 10:09:34 +0500

    solve: 2962 (js)

---
+++ solutions/2962-count-subarrays-where-max-element-appears-at-least-k-times/sliding-window.js
@@ -0,0 +1,23 @@
+/*
+ * @title: Sliding Window
+ * @time: O(n ^ 2)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countSubarrays = (nums, k) => {
+    const max = Math.max(...nums);
+    let count = 0,
+        left = 0,
+        res = 0;
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] == max) count++;
+        while (count == k) if (nums[left++] == max) count--;
+        res += left;
+    }
+    return res;
+};
