004f02baf64abe509ac77f79579838516bf516f2
Khumoyun <contact@xumoyun.uz>
2025-04-30 08:51:35 +0500

    solve: 0443 (js)

---
+++ solutions/0443-string-compression/two-pointers.js
@@ -0,0 +1,26 @@
+/*
+ * @title: Two Pointers
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+const compress = chars => {
+    let i = 0, k = 0;
+    while (i < chars.length) {
+        let count = 0;
+        while (i + count < chars.length && chars[i] === chars[i + count])
+            count++;
+
+        chars[k++] = chars[i];
+
+        if (count > 1) 
+            for (const c of String(count)) chars[k++] = c;
+
+        i += count;
+    }
+    return k;
+};
