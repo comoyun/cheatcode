eee78b1d73dbed02c09c9af7f24ed57023d94442
Khumoyun <contact@xumoyun.uz>
2025-04-30 08:16:00 +0500

    solve: 1295 (js)

---
+++ solutions/1295-find-numbers-with-even-number-of-digits/array.js
@@ -0,0 +1,19 @@
+/*
+ * @title: Array
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findNumbers = nums => {
+    let evenCount = 0;
+    for (const num of nums) {
+        if (num > 9 && num < 100) evenCount++;
+        if (num > 999 && num < 10000) evenCount++;
+        if (num > 99999 && num < 1000000) evenCount++;
+    }
+    return evenCount;
+};
---
+++ solutions/1295-find-numbers-with-even-number-of-digits/math.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Math
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findNumbers = nums => {
+    let evenCount = 0;
+
+    const getCount = num => {
+        let count = 0;
+        while (num) {
+            count++;
+            num = Math.floor(num / 10);
+        }
+        return count;
+    };
+
+    for (const num of nums) {
+        /*
+         * You can also use Math.floor(Math.log10(num))
+         * const count = Math.floor(Math.log10(num)) + 1;
+         */
+        const count = getCount(num);
+        if (count & 1) continue;
+        evenCount++;
+    }
+
+    return evenCount;
+};
