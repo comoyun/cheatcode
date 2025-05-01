4d97e23829bd3094c7d87853aa3eb53fbe44fcb9
Khumoyun <contact@xumoyun.uz>
2025-05-01 19:29:14 +0500

    solve: 0283 (js)

---
+++ solutions/0283-move-zeroes/two-pointers.js
@@ -0,0 +1,26 @@
+/*
+ * @title: Two Pointers
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = nums => {
+    let ptr1 = 0,
+        ptr2 = 0;
+    while (ptr1 < nums.length) {
+        if (ptr2 >= nums.length) {
+            nums[ptr1++] = 0;
+            continue;
+        }
+        if (nums[ptr2] === 0) {
+            ptr2++;
+            continue;
+        }
+        nums[ptr1++] = nums[ptr2++];
+    }
+    return nums;
+};
