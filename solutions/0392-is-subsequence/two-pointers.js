9a191f758d6acab82cb79f983e3cf41ee9c96745
Khumoyun <contact@xumoyun.uz>
2025-05-03 00:26:57 +0500

    solve: 0392 (js)

---
+++ solutions/0392-is-subsequence/two-pointers.js
@@ -0,0 +1,21 @@
+/*
+ * @title: Two Pointers
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isSubsequence = (s, t) => {
+    let a = 0;
+    for (const bc of t) {
+        const ac = s[a];
+        if (ac === bc) a++;
+        if (a === s.length) break;
+    }
+    return a === s.length;
+};
+
