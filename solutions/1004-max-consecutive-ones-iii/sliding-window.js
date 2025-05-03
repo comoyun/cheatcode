cf1c17ad2911d3123aa0ca0880e603628683acf6
Khumoyun <contact@xumoyun.uz>
2025-05-03 14:49:15 +0500

    solve: 1004 (js)

---
+++ solutions/1004-max-consecutive-ones-iii/sliding-window.js
@@ -0,0 +1,27 @@
+/*
+ * @title: Sliding Window
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const longestOnes = (nums, k) => {
+    let left = 0,
+        right = 0,
+        count = 0,
+        longest = 0;
+    for (; right < nums.length; right++) {
+        let length = right - left + 1;
+        if (nums[right]) count++;
+        while (count + k < length) {
+            if (nums[left++]) count--;
+            length = right - left + 1;
+        }
+        longest = Math.max(longest, Math.min(count + k, nums.length));
+    }
+    return longest;
+};
