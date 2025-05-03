078fbed8172b0693eba3667040db0209b8a513e6
Khumoyun <contact@xumoyun.uz>
2025-05-03 15:52:21 +0500

    solve: 1493 (js)

---
+++ solutions/1493-longest-subarray-of-1s-after-deleting-one-element/kadanes-algorithm.js
@@ -0,0 +1,28 @@
+/*
+ * @title: Kadane's Algorithm
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestSubarray = nums => {
+    let maxLen = 0,
+        curr = 0,
+        prev = 0,
+        seenZero = false;
+    for (let i = 0; i <= nums.length; i++) {
+        let num = nums[i] ?? 0;
+        if (num) {
+            curr++;
+            continue;
+        }
+        seenZero = seenZero || i < nums.length;
+        maxLen = Math.max(maxLen, curr + prev);
+        prev = curr;
+        curr = 0;
+    }
+    return seenZero ? maxLen : nums.length - 1;
+};
