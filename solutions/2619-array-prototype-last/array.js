0a8a4aa36d6177136330d7d7bf0467558d3383df
Khumoyun <contact@xumoyun.uz>
2025-05-04 22:18:18 +0500

    solve: 2619 (js)

---
+++ solutions/2619-array-prototype-last/array.js
@@ -0,0 +1,12 @@
+/*
+ * @title: Array
+ * @time: O(1)
+ * @space: O(1)
+ */
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+    return !this.length ? -1 : this[this.length - 1];
+};
