9eb78ac5d06a9016790f757c1059bdd518b8d903
Khumoyun <contact@xumoyun.uz>
2025-05-04 19:59:55 +0500

    solve: 1732 (js)

---
+++ solutions/1732-find-the-highest-altitude/prefix-sum.js
@@ -0,0 +1,19 @@
+/*
+ * @title: Prefix Sum
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+const largestAltitude = gains => {
+    let result = 0,
+        sum = 0;
+    for (const gain of gains) {
+        sum += gain;
+        result = Math.max(result, sum);
+    }
+    return result;
+};
