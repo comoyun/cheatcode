ec0c7e24ecb51d352100dcc6b2f3c49f6a0d29d2
Khumoyun <contact@xumoyun.uz>
2025-05-09 22:49:51 +0500

    solve: 0941 (js)

---
+++ solutions/0941-valid-mountain-array/array.js
@@ -0,0 +1,20 @@
+/*
+ * @title: Array
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const validMountainArray = a => {
+    let i = 0, n = a.length;
+    if (n < 3) return false;
+    while (i + 1 < n && a[i] < a[i + 1]) i++;
+    if (i === 0 || i === n - 1) return false;
+    while (i + 1 < n && a[i] > a[i + 1]) i++;
+    return i === n - 1;
+};
+
+console.log(validMountainArray([1, 2, 3, 2, 1]));
