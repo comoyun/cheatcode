eb448213fa93b071b4ffd2f19366634dd95227d4
Khumoyun <contact@xumoyun.uz>
2025-05-09 22:23:34 +0500

    solve: 404 (js)

---
+++ solutions/0404-sum-of-left-leaves/dfs.js
@@ -0,0 +1,18 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves = root => {
+    const dfs = (node, left = false) => {
+        if (!node) return 0;
+        if (!node.left && !node.right && left) return node.val;
+        return dfs(node.left, true) + dfs(node.right, false);
+    };
+    return dfs(root);
+};
