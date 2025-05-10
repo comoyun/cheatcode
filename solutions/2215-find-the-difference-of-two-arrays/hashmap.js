6984be703203bca3d17aabda2b4d6e8f4729f82b
Khumoyun <contact@xumoyun.uz>
2025-05-10 19:25:57 +0500

    solve: 2215 (js)

---
+++ solutions/2215-find-the-difference-of-two-arrays/hashmap.js
@@ -0,0 +1,26 @@
+/*
+ * @title: Hashmap
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+const findDifference = (nums1, nums2) => {
+    const seen1 = {},
+          seen2 = {};
+    const res = [[], []];
+
+    for (const n of nums1) seen1[n] = true;
+    for (const n of nums2) seen2[n] = true;
+
+    for (const n in seen1) 
+        if (!(n in seen2)) res[0].push(+n);
+    for (const n in seen2)
+        if (!(n in seen1)) res[1].push(+n);
+
+    return res;
+};
---
+++ solutions/2215-find-the-difference-of-two-arrays/two-pointers.js
@@ -0,0 +1,47 @@
+/*
+ * @title: Two Pointers
+ * @time: O(n log n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+const findDifference = (nums1, nums2) => {
+    const removeDuplicates = arr => {
+        let j = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (i > 0 && arr[i] === arr[i - 1]) continue;
+            arr[j++] = arr[i];
+        }
+        return j;
+    };
+
+    nums1.sort((a, b) => a - b);
+    nums1.length = removeDuplicates(nums1);
+
+    nums2.sort((a, b) => a - b);
+    nums2.length = removeDuplicates(nums2);
+
+    const result = [[], []];
+
+    let i = 0,
+        j = 0;
+    while (i < nums1.length || j < nums2.length) {
+        const a = i < nums1.length ? nums1[i] : Infinity,
+            b = j < nums2.length ? nums2[j] : Infinity;
+        if (a < b) {
+            result[0].push(a);
+            i++;
+        } else if (a > b) {
+            result[1].push(b);
+            j++;
+        } else {
+            i++;
+            j++;
+        }
+    }
+    return result;
+};
