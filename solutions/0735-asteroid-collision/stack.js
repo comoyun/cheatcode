45c6f2f013c310eedf7172a375341d2346198c2d
Khumoyun <contact@xumoyun.uz>
2025-05-12 00:39:06 +0500

    solve: 0735 (js)

---
+++ solutions/0735-asteroid-collision/stack.js
@@ -0,0 +1,33 @@
+/*
+ * @title: Stack
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+const asteroidCollision = asteroids => {
+    const stack = [];
+    for (let asteroid of asteroids) {
+        let add = true;
+        while (stack.length && stack.at(-1) > 0 && asteroid < 0) {
+            if (Math.abs(asteroid) > stack.at(-1)) {
+                stack.pop();
+                continue;
+            }
+            if (Math.abs(asteroid) === stack.at(-1)) {
+                stack.pop();
+                add = false;
+                break;
+            }
+            if (Math.abs(asteroid) < stack.at(-1)) {
+                add = false;
+                break;
+            }
+        }
+        if (add) stack.push(asteroid);
+    }
+    return stack;
+};
