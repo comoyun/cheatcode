c6325f534216d5ce00f4b2bb0feb0724e3152b88
Khumoyun <contact@xumoyun.uz>
2025-05-12 02:00:25 +0500

    solve: 0394 (js)

---
+++ solutions/0394-decode-string/stack.js
@@ -0,0 +1,31 @@
+/*
+ * @title: Stack
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const decodeString = s => {
+    const stack = [];
+    let num = '',
+        result = '';
+    for (const char of s) {
+        if (!isNaN(char)) num += char;
+        else if (char === '[') {
+            stack.push({ count: +num, str: '' });
+            num = '';
+        } else if (char === ']') {
+            const { count, str } = stack.pop();
+            const expanded = str.repeat(count);
+            if (stack.length) stack[stack.length - 1].str += expanded;
+            else result += expanded;
+        } else {
+            if (stack.length) stack[stack.length - 1].str += char;
+            else result += char;
+        }
+    }
+    return result;
+};
