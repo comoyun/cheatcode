02969338e0e58643b8b646dd3744d3e26202d84e
Khumoyun <contact@xumoyun.uz>
2025-05-17 09:49:49 +0500

    solve: 0075 (js)

---
+++ solutions/0075-sort-colors/two-pointers.js
@@ -0,0 +1,22 @@
+/*
+ * @title: Two-pointers
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors = nums => {
+    let red = 0, white = 0, blue = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) red++;
+        else if (nums[i] === 1) white++;
+        else blue++;
+    }
+    let i = 0, j = 0;
+    for (; i < red; i++) nums[j++] = 0;
+    for (i = 0; i < white; i++) nums[j++] = 1;
+    for (i = 0; i < blue; i++) nums[j++] = 2;
+};
