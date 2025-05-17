624657c407cad87190d0e86880b6802beec8ff4f
Khumoyun <contact@xumoyun.uz>
2025-05-17 13:28:38 +0500

    solve: 0437 (js)

---
+++ solutions/0437-path-sum-iii/dfs.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Dfs
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+const pathSum = (root, targetSum) => {
+    let count = 0;
+
+    const dfs = (node, currPath) => {
+        if (!node) return;
+
+        currPath.push(node.val);
+
+        let tempSum = 0;
+        for (let i = currPath.length - 1; i >= 0; i--) {
+            tempSum += currPath[i];
+            if (tempSum === targetSum) count++;
+        }
+
+        dfs(node.left, currPath);
+        dfs(node.right, currPath);
+
+        currPath.pop();
+    };
+
+    dfs(root, []);
+    return count;
+};
