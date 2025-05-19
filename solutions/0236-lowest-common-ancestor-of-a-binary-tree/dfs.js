b819cc56f0a484d149be8f273a9412cf0a077027
Khumoyun <contact@xumoyun.uz>
2025-05-19 14:56:00 +0500

    solve: 236 (js)

---
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/dfs.js
@@ -0,0 +1,24 @@
+/*
+ * @title: Dfs
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = (root, p, q) => {
+    let result;
+    const dfs = node => {
+        if (!node) return 0;
+        let sum = dfs(node.left) + dfs(node.right);
+        if (node === p || node === q) sum++;
+        if (sum === 2 && !result) result = node;
+        return sum;
+    };
+    dfs(root);
+    return result;
+};
