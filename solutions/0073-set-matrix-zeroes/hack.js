8fd11b47d291f74f09353acbbba6adeaf488b8ce
Khumoyun <contact@xumoyun.uz>
2025-05-21 19:43:50 +0500

    solve: 0073 (js)

---
+++ solutions/0073-set-matrix-zeroes/hack.js
@@ -0,0 +1,32 @@
+/*
+ * @title: Hack (Marker)
+ * @time: O(nm)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const setZeroes = matrix => {
+    const ROWS = matrix.length,
+        COLS = matrix[0].length,
+        MARK = Infinity;
+
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (!matrix[r][c]) {
+                for (let col = 0; col < COLS; col++)
+                    if (matrix[r][col]) matrix[r][col] = MARK;
+                for (let row = 0; row < ROWS; row++)
+                    if (matrix[row][c]) matrix[row][c] = MARK;
+            }
+        }
+    }
+
+    for (let r = 0; r < ROWS; r++)
+        for (let c = 0; c < COLS; c++)
+            if (matrix[r][c] === MARK) matrix[r][c] = 0;
+
+    return matrix;
+};
---
+++ solutions/0073-set-matrix-zeroes/hashmap.js
@@ -0,0 +1,32 @@
+/*
+ * @title: Hashmap
+ * @time: O(nm)
+ * @space: O(n + m)
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const setZeroes = matrix => {
+    const zeroCols = new Set(),
+        zeroRows = new Set();
+    const ROWS = matrix.length,
+        COLS = matrix[0].length;
+
+    for (let row = 0; row < ROWS; row++) {
+        for (let col = 0; col < COLS; col++) {
+            if (!matrix[row][col]) {
+                zeroCols.add(col);
+                zeroRows.add(row);
+            }
+        }
+    }
+
+    for (let row = 0; row < ROWS; row++)
+        for (let col = 0; col < COLS; col++)
+            if (zeroRows.has(row) || zeroCols.has(col)) 
+                matrix[row][col] = 0;
+
+    return matrix;
+};
