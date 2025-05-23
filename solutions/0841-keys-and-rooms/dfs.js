c54fafbe3233ac53a41e17171ef8278d9938002b
Khumoyun <contact@xumoyun.uz>
2025-05-23 23:50:05 +0500

    solve: 0841 (js)

---
+++ solutions/0841-keys-and-rooms/dfs.js
@@ -0,0 +1,23 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+const canVisitAllRooms = rooms => {
+    const seen = new Set();
+    const dfs = start => {
+        seen.add(start);
+        for (let i = 0; i < rooms[start].length; i++) {
+            const key = rooms[start][i];
+            if (seen.has(key)) continue;
+            dfs(key);
+        }
+    };
+    dfs(0);
+    return seen.size === rooms.length;
+};
