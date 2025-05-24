2293fe7b31affa05430aa415113ed1401753731b
Khumoyun <contact@xumoyun.uz>
2025-05-24 23:13:47 +0500

    solve: 0071 (js)

---
+++ solutions/0071-simplify-path/stack.js
@@ -0,0 +1,22 @@
+/*
+ * @title: Stack
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} path
+ * @return {string}
+ */
+const simplifyPath = path => {
+    const parts = path.split('/'),
+        stack = [];
+
+    for (const part of parts) {
+        if (part === '' || part === '.') continue;
+        if (part === '..') stack.pop();
+        else stack.push(part);
+    }
+
+    return '/' + stack.join('/');
+};
