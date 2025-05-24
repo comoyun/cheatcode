d53ebcca2b4674bbc1b47f44d7c5e979e4b1eb18
Khumoyun <contact@xumoyun.uz>
2025-05-24 15:24:16 +0500

    solve: 2942 (js)

---
+++ solutions/2942-find-words-containing-character/array.js
@@ -0,0 +1,25 @@
+/*
+ * @title: Array
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+const findWordsContaining = (words, x) => {
+    const result = [];
+
+    /* NOTE: You can use str.indexOf or str.includes functions */
+    const contains = str => {
+        for (const char of str) if (char === x) return true;
+        return false;
+    };
+
+    for (let i = 0; i < words.length; i++)
+        if (contains(words[i])) result.push(i);
+
+    return result;
+};
