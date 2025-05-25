6017114ca05444dd94b5fe80912e8c1fb3d39cf7
Khumoyun <contact@xumoyun.uz>
2025-05-25 11:00:12 +0500

    solve: 0547 (js)

---
+++ solutions/0547-number-of-provinces/dfs.js
@@ -0,0 +1,31 @@
+/*
+ * @title: DFS
+ * @time: O(n^2)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+const findCircleNum = isConnected => {
+    const n = isConnected.length;
+    const visited = new Array(n).fill(false);
+    let count = 0;
+
+    const dfs = city => {
+        visited[city] = true;
+        for (let neighbor = 0; neighbor < n; neighbor++)
+            if (isConnected[city][neighbor] && !visited[neighbor])
+                dfs(neighbor);
+    };
+
+    for (let city = 0; city < n; city++) {
+        if (!visited[city]) {
+            dfs(city);
+            count++;
+        }
+    }
+
+    return count;
+};
