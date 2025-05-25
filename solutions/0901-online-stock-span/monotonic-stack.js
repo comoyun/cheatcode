223a2b3383d000120c5700bf5ab115480cbfc573
Khumoyun <contact@xumoyun.uz>
2025-05-25 17:55:12 +0500

    solve: 0901 (js)

---
+++ solutions/0901-online-stock-span/monotonic-stack.js
@@ -0,0 +1,23 @@
+/*
+ * @title: Monotonic Stack
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+class StockSpanner {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} price
+     * @return {number}
+     */
+    next(price) {
+        let span = 1;
+        while (this.stack.length && this.stack.at(-1)[0] <= price)
+            span += this.stack.pop()[1];
+        this.stack.push([price, span]);
+        return span;
+    }
+}
