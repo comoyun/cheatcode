3a73bb5daf0c6013729e321bd1569da81d433963
Khumoyun <contact@xumoyun.uz>
2025-05-25 14:23:13 +0500

    solve: 0739 (js)

---
+++ solutions/0739-daily-temperatures/monotonic-stack.js
@@ -0,0 +1,24 @@
+/*
+ * @title: Monotonic Stack
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+const dailyTemperatures = temps => {
+    const result = new Array(temps.length).fill(0);
+    const stack = [];
+
+    for (let i = temps.length - 1; i >= 0; i--) {
+        while (stack.length && temps[stack.at(-1)] <= temps[i]) stack.pop();
+
+        if (stack.length) result[i] = stack[stack.length - 1] - i;
+
+        stack.push(i);
+    }
+
+    return result;
+};
