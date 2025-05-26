51ecfaab7c39e3e12a64ba18d08401efff2695d6
Khumoyun <contact@xumoyun.uz>
2025-05-26 12:48:23 +0500

    solve: 2721 (js)

---
+++ solutions/2721-execute-asynchronous-functions-in-parallel/promise.js
@@ -0,0 +1,26 @@
+/*
+ * @title: Promise
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+const promiseAll = functions => {
+    const length = functions.length,
+        result = new Array(length);
+
+    return new Promise((resolve, reject) => {
+        let count = 0;
+        for (let i = 0; i < length; i++) {
+            functions[i]()
+                .then(data => {
+                    result[i] = data;
+                    if (++count === length) resolve(result);
+                })
+                .catch(reject);
+        }
+    });
+};
