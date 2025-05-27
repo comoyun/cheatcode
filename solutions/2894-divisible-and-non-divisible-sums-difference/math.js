5b9ffdd0a9d084361f95e872c2d367ea554ed766
Khumoyun <contact@xumoyun.uz>
2025-05-27 09:10:38 +0500

    solve: 2894 (js)

---
+++ solutions/2894-divisible-and-non-divisible-sums-difference/math.js
@@ -0,0 +1,21 @@
+/*
+ * @title: Math
+ * @time: O(1)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+const differenceOfSums = (n, m) => {
+    const total = (n * (n + 1)) / 2;
+    const count = Math.floor(n / m);
+    const div = m * (count * (count + 1)) / 2;
+    const res = total - div * 2;
+
+    return res;
+};
+
+
