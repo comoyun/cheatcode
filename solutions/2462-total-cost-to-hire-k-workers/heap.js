2cd8749d9c71ff56387913b66020c222bd8e6fb1
Khumoyun <contact@xumoyun.uz>
2025-05-29 16:44:50 +0500

    solve: 2462 (js)

---
+++ solutions/2462-total-cost-to-hire-k-workers/heap.js
@@ -0,0 +1,50 @@
+/*
+ * @title: Heap
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} costs
+ * @param {number} k
+ * @param {number} candidates
+ * @return {number}
+ */
+const totalCost = (costs, k, candidates) => {
+    const n = costs.length;
+    let left = 0,
+        right = n - 1,
+        total = 0;
+
+    const leftHeap = new Heap((a, b) => costs[a] - costs[b]),
+        rightHeap = new Heap((a, b) => costs[a] - costs[b]);
+
+    if (2 * candidates >= n) {
+        costs.sort((a, b) => a - b);
+        for (let i = 0; i < k; i++) total += costs[i];
+        return total;
+    }
+
+    for (let i = 0; i < candidates; i++) {
+        leftHeap.insert(left++);
+        rightHeap.insert(right--);
+    }
+
+    for (let hire = 0; hire < k; hire++) {
+        const lmin =
+            leftHeap.peek() !== null ? costs[leftHeap.peek()] : Infinity;
+        const rmin =
+            rightHeap.peek() !== null ? costs[rightHeap.peek()] : Infinity;
+
+        if (lmin <= rmin) {
+            total += costs[leftHeap.remove()];
+            if (left <= right) leftHeap.insert(left++);
+        } else {
+            total += costs[rightHeap.remove()];
+            if (left <= right) rightHeap.insert(right--);
+        }
+    }
+
+    return total;
+};
+
