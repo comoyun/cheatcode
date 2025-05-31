8b48494c5b4b5d8eb377c60e3a99849a95a5f3d4
Khumoyun <contact@xumoyun.uz>
2025-05-31 23:42:28 +0500

    solve: 2694 (js)

---
+++ solutions/2694-event-emitter/design.js
@@ -0,0 +1,56 @@
+/*
+ * @title: Design
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+        this.uid = 0;
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        const uid = this.uid++;
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push([callback, uid]);
+        return {
+            unsubscribe: () => {
+                const arr = this.events[eventName];
+                const remIdx = this.binarySearch(arr, uid);
+                if (remIdx === -1) return;
+                this.events[eventName].splice(remIdx, 1);
+            },
+        };
+    }
+
+    binarySearch(arr, target) {
+        let left = 0,
+            right = arr.length - 1;
+        while (left <= right) {
+            const mid = left + Math.floor((right - left) / 2);
+            const item = arr[mid][1];
+            if (item > target) right = mid - 1;
+            else if (item < target) left = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        const callbacks = this.events[eventName] || [];
+        const result = [];
+        for (const callback of callbacks) result.push(callback[0](...args));
+        return result;
+    }
+}
