3ef67db67413ecf36229a9c3cbfafef517e33a32
Khumoyun <contact@xumoyun.uz>
2025-06-02 00:27:20 +0500

    solve: 2109 (js)

---
+++ solutions/2109-adding-spaces-to-a-string/array.js
@@ -0,0 +1,16 @@
+/*
+ * @title: Simulation
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+const addSpaces = (s, spaces) => {
+    s = s.split('');
+    for (const space of spaces) s[space] = ' ' + s[space];
+    return s.join('');
+};
---
+++ solutions/2109-adding-spaces-to-a-string/simulation.js
@@ -0,0 +1,23 @@
+/*
+ * @title: Simulation
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+const addSpaces = (s, spaces) => {
+    const result = [];
+    let j = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (spaces[j] === i) {
+            result.push(' ');
+            j++;
+        }
+        result.push(s[i]);
+    }
+    return result.join('');
+};
