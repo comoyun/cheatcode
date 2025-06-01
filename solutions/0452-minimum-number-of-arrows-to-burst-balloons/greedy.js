cc00ea23b75bb5c4ac0b1609719016c14b053e74
Khumoyun <contact@xumoyun.uz>
2025-06-01 23:21:36 +0500

    solve: 0452 (js)

---
+++ solutions/0452-minimum-number-of-arrows-to-burst-balloons/greedy.js
@@ -0,0 +1,23 @@
+/*
+ * @title: Greedy
+ * @time: O(n log n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+const findMinArrowShots = points => {
+    points.sort((a, b) => a[1] - b[1]);
+    let arrows = 1;
+    let prevEnd = points[0][1];
+    for (let i = 1; i < points.length; i++) {
+        const [start, end] = points[i];
+        if (start > prevEnd) {
+            arrows++;
+            prevEnd = end;
+        }
+    }
+    return arrows;
+};
