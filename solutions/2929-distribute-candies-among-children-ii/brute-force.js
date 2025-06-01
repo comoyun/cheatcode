d4ae6c890c3cb77a3307d92b9cfe4287fa016bb9
Khumoyun <contact@xumoyun.uz>
2025-06-01 21:46:09 +0500

    solve: 2929 (js)

---
+++ solutions/2929-distribute-candies-among-children-ii/brute-force.js
@@ -0,0 +1,48 @@
+/*
+ * @title: Brute-force
+ * @time: O(n^3)
+ * @space: O(1)
+ */
+
+/* WARNING: These solutions give TLE */
+
+/**
+ * @param {number} n
+ * @param {number} limit
+ * @return {number}
+ */
+const distributeCandies = (n, limit) => {
+    let count = 0;
+    limit = Math.min(n, limit);
+    for (let i = 0; i <= limit; i++) {
+        for (let j = 0; j <= limit; j++) {
+            for (let k = 0; k <= limit; k++) {
+                const sum = i + j + k;
+                if (sum === n) {
+                    count++;
+                    console.log(i, j, k);
+                }
+            }
+        }
+    }
+    return count;
+};
+
+/*
+ * @title: Optimized brute-force
+ * @time: O(n^2)
+ * @space: O(1)
+ */
+const distributeCandiesV2 = (n, limit) => {
+    let count = 0;
+    limit = Math.min(n, limit);
+    for (let i = 0; i <= limit; i++) {
+        for (let j = 0; j <= limit; j++) {
+            const sum = i + j;
+            const compliment = n - sum;
+            if (compliment > limit || compliment < 0) continue;
+            count++;
+        }
+    }
+    return count;
+};
