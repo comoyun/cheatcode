a5686ba595047dd77a71205ae3dd186a7bba7115
Khumoyun <contact@xumoyun.uz>
2025-06-02 21:42:15 +0500

    perf: calculate sum in second pass

--- solutions/0135-candy/greedy.js
+++ solutions/0135-candy/greedy.js
@@ -11,13 +11,16 @@
 const candy = ratings => {
     const n = ratings.length;
     const candies = new Array(n).fill(1);
+    let sum = 0;
 
     for (let i = 1; i < n; i++)
         if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1;
 
-    for (let i = n - 2; i >= 0; i--)
+    for (let i = n - 2; i >= 0; i--) {
         if (ratings[i] > ratings[i + 1])
             candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        sum += candies[i];
+    }
 
-    return candies.reduce((a, b) => a + b, 0);
+    return sum;
 };
