135da00f3e2768ba403c9bc65fc763238602f2c3
Khumoyun <contact@xumoyun.uz>
2025-06-08 17:15:04 +0500

    solve: 0226 (js)

---
+++ solutions/0226-invert-binary-tree/dfs.js
@@ -0,0 +1,25 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = root => {
+    if (!root) return null;
+    const dfs = node => {
+        if (!node) return;
+
+        const temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        dfs(node.left);
+        dfs(node.right);
+    };
+    dfs(root);
+    return root;
+};
