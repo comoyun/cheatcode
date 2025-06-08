967097e31f1152b35ea1eea471ce5a0ba238bb3c
Khumoyun <contact@xumoyun.uz>
2025-06-08 16:25:08 +0500

    solve: 1022 (js)

---
+++ solutions/1022-sum-of-root-to-leaf-binary-numbers/dfs.js
@@ -0,0 +1,19 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumRootToLeaf = root => {
+    const dfs = (node, curr) => {
+        if (!node) return 0;
+        curr = (curr << 1) | node.val;
+        if (!node.left && !node.right) return curr;
+        return dfs(node.left, curr) + dfs(node.right, curr);
+    };
+    return dfs(root, 0);
+};
