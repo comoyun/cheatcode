eab56cb869ee02fbb77da2e41fd9cd73070ba1f0
Khumoyun <contact@xumoyun.uz>
2025-06-08 20:46:41 +0500

    solve: 0232 (js)

---
+++ solutions/0232-implement-queue-using-stacks/lazy-queue.js
@@ -0,0 +1,51 @@
+/*
+ * @title: Lazy Queue
+ * @time: O(1)
+ * @space: O(n)
+ */
+
+class MyQueue {
+    constructor() {
+        this.stack = [];
+        this.idx = 0;
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+        this.stack.push(x);
+
+        if (this.idx > 1000) {
+            let i = 0;
+            while (this.idx < this.stack.length)
+                this.stack[i++] = this.stack[this.idx++];
+            this.stack.length = i;
+            this.idx = 0;
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        if (this.idx >= this.stack.length) return;
+        return this.stack[this.idx++];
+    }
+
+    /**
+     * @return {number}
+     */
+    peek() {
+        return this.stack[this.idx];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty() {
+        const length = this.stack.length;
+        return this.idx >= length;
+    }
+}
---
+++ solutions/0232-implement-queue-using-stacks/stack.js
@@ -0,0 +1,45 @@
+/*
+ * @title: Stack
+ * @time: O(1)
+ * @space: O(n)
+ */
+
+class MyQueue {
+    constructor() {
+        this.inStack = [];
+        this.outStack = [];
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+        this.inStack.push(x);
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        if (!this.outStack.length)
+            while (this.inStack.length) this.outStack.push(this.inStack.pop());
+        return this.outStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    peek() {
+        if (!this.outStack.length)
+            while (this.inStack.length) this.outStack.push(this.inStack.pop());
+        return this.outStack.length && this.outStack.at(-1);
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty() {
+        return this.inStack.length + this.outStack.length === 0;
+    }
+}
