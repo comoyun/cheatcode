c8774811cbd866c74c9fbd119affe3c3a6c09d6a
Khumoyun <contact@xumoyun.uz>
2025-06-08 08:41:58 +0500

    solve: 0091 (js)

---
+++ solutions/0091-decode-ways/dfs.js
@@ -0,0 +1,27 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numDecodings = s => {
+    const memo = {};
+
+    const dfs = idx => {
+        if (idx === s.length) return 1;
+        if (s[idx] === '0') return 0;
+        if (idx in memo) return memo[idx];
+
+        let count = dfs(idx + 1);
+        if (idx + 1 < s.length && +s.slice(idx, idx + 2) <= 26)
+            count += dfs(idx + 2);
+
+        return (memo[idx] = count);
+    };
+
+    return dfs(0);
+};
