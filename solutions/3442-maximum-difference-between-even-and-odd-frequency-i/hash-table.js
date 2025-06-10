25bb4690e512052049b8900f47e17788282489e8
Khumoyun <contact@xumoyun.uz>
2025-06-10 12:17:55 +0500

    solve: 3442 (js)

---
+++ solutions/3442-maximum-difference-between-even-and-odd-frequency-i/hash-table.js
@@ -0,0 +1,32 @@
+/*
+ * @title: Hash-table
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maxDifference = s => {
+    let oddMax, evenMin;
+    const freq = new Array(26).fill(0);
+    const getId = c => c.charCodeAt(0) - 97;
+
+    evenMin = Infinity;
+    oddMax = -Infinity;
+
+    for (const char of s) freq[getId(char)]++;
+
+    for (const count of freq) {
+        if (!count) continue;
+        const isOdd = count & 1;
+        if (isOdd) {
+            oddMax = Math.max(oddMax, count);
+            continue;
+        }
+        evenMin = Math.min(evenMin, count);
+    }
+
+    return oddMax - evenMin;
+};
