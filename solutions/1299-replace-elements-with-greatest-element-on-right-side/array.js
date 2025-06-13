cbbbbbad0b55853258c26cb3e8da0729843e4b6e
Khumoyun <contact@xumoyun.uz>
2025-06-13 21:45:03 +0500

    solve: 1299 (js)

---
+++ solutions/1299-replace-elements-with-greatest-element-on-right-side/array.js
@@ -0,0 +1,19 @@
+/*
+ * @title: Array
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+const replaceElements = arr => {
+    let max = -1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const num = arr[i];
+        arr[i] = max;
+        max = Math.max(max, num);
+    }
+    return arr;
+};
