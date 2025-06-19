21e8f3cb71217009ff01db2f515c0a19e4c1a145
Khumoyun <contact@xumoyun.uz>
2025-06-19 21:13:57 +0500

    solve: 2294 (js)

---
+++ solutions/2294-partition-array-such-that-maximum-difference-is-k/sorting.js
@@ -0,0 +1,24 @@
+/*
+ * @title: Sorting
+ * @time: O(n log n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const partitionArray = (nums, k) => {
+    nums.sort((a, b) => a - b);
+    let count = 1;
+    let min = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        const num = nums[i];
+        if (num - min > k) {
+            min = num;
+            count++;
+        }
+    }
+    return count;
+};
