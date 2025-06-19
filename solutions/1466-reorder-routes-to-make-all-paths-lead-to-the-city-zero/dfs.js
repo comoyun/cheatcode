cbb0c3acdf2ff2196e0032e61eac7b4d53df2666
Khumoyun <contact@xumoyun.uz>
2025-06-19 21:13:41 +0500

    solve: 1466 (js)

---
+++ solutions/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/dfs.js
@@ -0,0 +1,33 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+const minReorder = (n, connections) => {
+    const adj = Array.from({ length: n }, () => []);
+    for (const [u, v] of connections) {
+        adj[u].push([v, 1]); 
+        adj[v].push([u, 0]); 
+    }
+
+    let count = 0;
+    const visited = new Set();
+
+    const dfs = node => {
+        visited.add(node);
+        for (const [nei, needsReversal] of adj[node]) {
+            if (visited.has(nei)) continue;
+            count += needsReversal;
+            dfs(nei);
+        }
+    };
+
+    dfs(0);
+    return count;
+};
