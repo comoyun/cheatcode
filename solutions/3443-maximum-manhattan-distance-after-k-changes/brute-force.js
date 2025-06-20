0f687a42617904fb5864bebea97fbbea5380542c
Khumoyun <contact@xumoyun.uz>
2025-06-20 15:29:33 +0500

    solve: 3443 (js)

---
+++ solutions/3443-maximum-manhattan-distance-after-k-changes/brute-force.js
@@ -0,0 +1,31 @@
+/*
+ * @title: Brute Force
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const maxDistance = (s, k) => {
+    let max = 0;
+    const dirs = ['NE', 'NW', 'SE', 'SW'];
+    for (const dir of dirs) {
+        let remK = k,
+            dist = 0;
+        for (const char of s) {
+            if (dir.indexOf(char) !== -1) {
+                dist++;
+            } else if (remK) {
+                dist++;
+                remK--;
+            } else {
+                dist--;
+            }
+            max = Math.max(max, dist);
+        }
+    }
+    return max;
+};
