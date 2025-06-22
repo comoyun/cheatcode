be9e64b66c65b308d8132eb4bd2181aa19ac3aaa
Khumoyun <contact@xumoyun.uz>
2025-06-22 11:04:14 +0500

    solve: 2138 (js)

---
+++ solutions/2138-divide-a-string-into-groups-of-size-k/math.js
@@ -0,0 +1,18 @@
+/*
+ * @title: Math
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @param {character} fill
+ * @return {string[]}
+ */
+const divideString = (s, k, fill) =>
+    Array.from({ length: Math.ceil(s.length / k) }, (_, i) => {
+        const chunk = s.slice(i * k, (i + 1) * k);
+        return chunk.length < k ? chunk + fill.repeat(k - chunk.length) : chunk;
+    });
+
