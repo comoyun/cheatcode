d13a444d015022be5ea474db146e678d1e92fc53
Khumoyun <contact@xumoyun.uz>
2025-06-22 20:57:49 +0500

    solve: 0208 (js)

---
+++ solutions/0208-implement-trie-(prefix-tree)/design.js
@@ -0,0 +1,47 @@
+/*
+ * @title: Design
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+class Trie {
+    constructor() {
+        this.root = {};
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let curr = this.root;
+        for (const char of word) curr = curr[char] ??= {};
+        curr.isTerminal = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let curr = this.root;
+        for (const char of word) {
+            if (!(char in curr)) return false;
+            curr = curr[char];
+        }
+        return curr.isTerminal === true;
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let curr = this.root;
+        for (const char of prefix) {
+            if (!(char in curr)) return false;
+            curr = curr[char];
+        }
+        return true;
+    }
+}
