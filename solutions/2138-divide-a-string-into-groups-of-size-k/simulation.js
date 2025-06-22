72afdffb2403df480189480a5290fece1742a502
Khumoyun <contact@xumoyun.uz>
2025-06-22 11:04:36 +0500

    solve: 2138 (js)

---
+++ solutions/2138-divide-a-string-into-groups-of-size-k/simulation.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Simulation
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @param {character} fill
+ * @return {string[]}
+ */
+const divideString = (s, k, fill) => {
+    const result = [];
+    let group = [];
+    for (const char of s) {
+        group.push(char);
+
+        if (group.length === k) {
+            result.push(group.join(''));
+            group = [];
+        }
+    }
+
+    if (group.length && group.length < k) {
+        const diff = k - group.length;
+        const rem = fill.repeat(diff);
+        group = [...group, rem];
+        result.push(group.join(''));
+    }
+
+    return result;
+};
+
