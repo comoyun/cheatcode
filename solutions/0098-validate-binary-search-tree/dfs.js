37d7cd96cdffc3b8bf029c35997124232d40014d
Khumoyun <contact@xumoyun.uz>
2025-06-23 21:06:04 +0500

    solve: 0098 (js)

---
+++ solutions/0098-validate-binary-search-tree/dfs.js
@@ -0,0 +1,21 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = root => {
+    const dfs = (node, min = -Infinity, max = Infinity) => {
+        if (!node) return true;
+        const val = node.val;
+
+        if (val <= min || val >= max) return false;
+
+        return dfs(node.left, min, val) && dfs(node.right, val, max);
+    };
+    return dfs(root);
+};
