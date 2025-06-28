502fc943b9963ec8dad0ad767651b24712d0181a
Khumoyun <contact@xumoyun.uz>
2025-06-28 08:13:58 +0500

    solve: 0300 (js)

---
+++ solutions/0300-longest-increasing-subsequence/dynamic-programming.js
@@ -0,0 +1,22 @@
+/*
+ * @title: Type
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const lengthOfLIS = nums => {
+    const n = nums.length;
+    const dp = new Array(n).fill(0);
+    for (let i = n - 1; i >= 0; i--) {
+        let max = 1;
+        for (let j = i + 1; j < n; j++)
+            if (nums[i] < nums[j]) max = Math.max(max, 1 + dp[j]);
+        dp[i] = max;
+    }
+
+    return Math.max(...dp);
+};
