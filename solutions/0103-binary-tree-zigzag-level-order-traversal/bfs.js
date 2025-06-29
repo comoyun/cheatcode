e5b1388248f315167c656fae0777b8aa72abc7aa
Khumoyun <contact@xumoyun.uz>
2025-06-29 22:02:11 +0500

    solve: 0103 (js)

---
+++ solutions/0103-binary-tree-zigzag-level-order-traversal/bfs.js
@@ -0,0 +1,27 @@
+/*
+ * @title: BFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const zigzagLevelOrder = root => {
+    if (!root) return [];
+    const queue = [root];
+    const levels = [];
+    while (queue.length) {
+        const length = queue.length;
+        const level = [];
+        for (let i = 0; i < length; i++) {
+            const curr = queue.shift();
+            level.push(curr.val);
+            if (curr.left) queue.push(curr.left);
+            if (curr.right) queue.push(curr.right);
+        }
+        levels.push(level);
+    }
+    return levels.map((level, idx) => (idx & 1 ? level.reverse() : level));
+};
