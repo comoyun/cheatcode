64a34e4a3173c0d73abc2ed46052603a87600341
Khumoyun <contact@xumoyun.uz>
2025-06-29 23:18:20 +0500

    solve: 0106 (js)

---
+++ solutions/0106-construct-binary-tree-from-inorder-and-postorder-traversal/binary-tree.js
@@ -0,0 +1,30 @@
+/*
+ * @title: Binary Tree
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+const buildTree = (inorder, postorder) => {
+    let idx = postorder.length - 1;
+    const build = bank => {
+        if (idx < 0) return null;
+        if (!bank.length) return null;
+
+        const val = postorder[idx--];
+        const node = new TreeNode(val);
+
+        let i = bank.indexOf(val);
+
+        node.right = build(bank.slice(i + 1));
+        node.left = build(bank.slice(0, i));
+
+        return node;
+    };
+    return build(inorder);
+};
+
