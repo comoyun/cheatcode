9c4a1761fc7d4f6660cae68e14766976bffd37bc
Khumoyun <contact@xumoyun.uz>
2025-06-30 09:32:24 +0500

    chore: rename solution, trim header, fix casing

--- solutions/0104-maximum-depth-of-binary-tree/depth-first-search.js
+++ solutions/0104-maximum-depth-of-binary-tree/depth-first-search.js
@@ -1,17 +1,9 @@
 /*
- * @title: Dfs
+ * @title: DFS
  * @time: O(n)
  * @space: O(n)
  */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
 /**
  * @param {TreeNode} root
  * @return {number}
