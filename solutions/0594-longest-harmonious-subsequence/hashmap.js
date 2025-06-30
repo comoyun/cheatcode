fec8c4be89c0a2ee7c9e615558a0fedb2880daf4
Khumoyun <contact@xumoyun.uz>
2025-06-30 09:24:52 +0500

    solve: 0594 (js)

---
+++ solutions/0594-longest-harmonious-subsequence/hashmap.js
@@ -0,0 +1,20 @@
+/*
+ * @title: Hashmap
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findLHS = nums => {
+    const freq = {};
+    let result = 0;
+    for (const num of nums) freq[num] = (freq[num] || 0) + 1;
+    for (const num of nums) {
+        const next = num + 1;
+        if (freq[next]) result = Math.max(result, freq[next] + freq[num]);
+    }
+    return result;
+};
