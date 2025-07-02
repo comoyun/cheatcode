ceb2e624d1572281561428f560988784da8c8607
Khumoyun <contact@xumoyun.uz>
2025-07-02 18:32:03 +0500

    solve: 0129 (js)

---
+++ solutions/0129-sum-root-to-leaf-numbers/string.js
@@ -0,0 +1,21 @@
+/*
+ * @title: String
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumNumbers = root => {
+    const stack = [[root, root.val.toString()]];
+    let sum = 0;
+    while (stack.length) {
+        const [node, num] = stack.pop();
+        if (!node.left && !node.right) sum += parseInt(num);
+        if (node.left) stack.push([node.left, num + node.left.val]);
+        if (node.right) stack.push([node.right, num + node.right.val]);
+    }
+    return sum;
+};
