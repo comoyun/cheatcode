256a7c33460f349fe9abe4914a8a1ca2012d0e90
Khumoyun <contact@xumoyun.uz>
2025-07-02 15:56:30 +0500

    solve: 0129 (js)

---
+++ solutions/0129-sum-root-to-leaf-numbers/depth-first-search.js
@@ -0,0 +1,30 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumNumbers = root => {
+    let sum = 0;
+    let num = 0;
+    const dfs = node => {
+        if (!node) return;
+        num *= 10;
+        num += node.val;
+
+        if (!node.left && !node.right) sum += num;
+
+        dfs(node.left);
+        dfs(node.right);
+
+        num /= 10;
+        num = Math.floor(num);
+
+        return sum;
+    };
+    return dfs(root);
+};
