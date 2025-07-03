a55a13294d5e6a89da166a6d057f54f9d387aa0a
Khumoyun <contact@xumoyun.uz>
2025-07-03 09:50:42 +0500

    perf: pregenerate entire sequence

--- solutions/3304-find-the-k-th-character-in-string-game-i/simulation.js
+++ solutions/3304-find-the-k-th-character-in-string-game-i/simulation.js
@@ -4,20 +4,19 @@
  * @space: Exponential
  */
 
+const word = ['a'];
+while (word.length < 500) {
+    let len = word.length;
+    for (let i = 0; i < len; i++) {
+        let code = word[i].charCodeAt(0);
+        if (code === 122) code = 97;
+        else code++;
+        word.push(String.fromCharCode(code));
+    }
+}
+
 /**
  * @param {number} k
  * @return {character}
  */
-const kthCharacter = k => {
-    const word = ['a'];
-    while (word.length < k) {
-        let len = word.length;
-        for (let i = 0; i < len; i++) {
-            let code = word[i].charCodeAt(0);
-            if (code === 122) code = 97;
-            else code++;
-            word.push(String.fromCharCode(code));
-        }
-    }
-    return word[k - 1];
-};
+const kthCharacter = k => word[k - 1];
