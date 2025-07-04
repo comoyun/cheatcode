ba0a936f4a159febf92fe3c9b70e521f71c83733
Khumoyun <contact@xumoyun.uz>
2025-07-04 20:48:41 +0500

    solve: 0117 (js)

---
+++ solutions/0117-populating-next-right-pointers-in-each-node-ii/depth-first-search.js
@@ -0,0 +1,34 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+    if (!root) return null;
+
+    const dfs = (node, next) => {
+        if (!node) return;
+
+        node.next = next;
+
+        const getNext = n => {
+            while (n) {
+                if (n.left) return n.left;
+                if (n.right) return n.right;
+                n = n.next;
+            }
+            return null;
+        };
+
+        dfs(node.right, getNext(next));
+        dfs(node.left, node.right ? node.right : getNext(next));
+    };
+
+    dfs(root, null);
+    return root;
+};
