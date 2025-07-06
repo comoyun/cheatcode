acb59ac991c2d085b5204c30f83f9a5a6a7e71cd
Khumoyun <contact@xumoyun.uz>
2025-07-06 15:25:40 +0500

    solve: 1865 (js)

---
+++ solutions/1865-finding-pairs-with-a-certain-sum/hash-table.js
@@ -0,0 +1,48 @@
+/*
+ * @title: Hash Table
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+class FindSumPairs {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     */
+    constructor(nums1, nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+
+        this.freq2 = {};
+        for (const num of nums2) this.freq2[num] = (this.freq2[num] || 0) + 1;
+    }
+
+    /**
+     * @param {number} index
+     * @param {number} val
+     * @return {void}
+     */
+    add(index, val) {
+        const oldVal = this.nums2[index];
+        const newVal = oldVal + val;
+
+        this.freq2[oldVal]--;
+        this.freq2[newVal] = (this.freq2[newVal] || 0) + 1;
+        this.nums2[index] = newVal;
+    }
+
+    /**
+     * @param {number} tot
+     * @return {number}
+     */
+    count(tot) {
+        let count = 0;
+
+        for (const num of this.nums1) {
+            const complement = tot - num;
+            count += this.freq2[complement] || 0;
+        }
+
+        return count;
+    }
+}
