61904d08ce112d57688f836be4e4441467546435
Khumoyun <contact@xumoyun.uz>
2025-07-10 04:43:27 +0500

    solve: 0150 (js)

---
+++ solutions/0150-evaluate-reverse-polish-notation/recursion.js
@@ -0,0 +1,31 @@
+/*
+ * @title: Recursion
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+const evalRPN = tokens => {
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => Math.trunc(a / b),
+    };
+
+    const recur = () => {
+        const token = tokens.pop();
+        if (token in operators) {
+            const b = recur();
+            const a = recur();
+            return operators[token](a, b);
+        }
+
+        return Number(token);
+    };
+
+    return recur();
+};
---
+++ solutions/0150-evaluate-reverse-polish-notation/stack.js
@@ -0,0 +1,31 @@
+/*
+ * @title: Stack
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+const evalRPN = tokens => {
+    const stack = [];
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => Math.trunc(a / b)
+    };
+    for (const char of tokens) {
+        if (char in operators) {
+            const b = Math.abs(stack.pop());
+            const a = Math.abs(stack.pop());
+            stack.push(operators[char](a, b));
+            continue;
+        }
+        stack.push(Number(char));
+    }
+
+    return stack[0];
+};
+
