e3c0370b6a82f34424a640245ee329e6efcfeb01
Khumoyun <contact@xumoyun.uz>
2025-07-13 00:53:59 +0500

    solve: 0209 (js)

---
+++ solutions/0209-minimum-size-subarray-sum/sliding-window.js
@@ -0,0 +1,29 @@
+/*
+ * @title: Sliding Window
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minSubArrayLen = (target, nums) => {
+    const n = nums.length;
+
+    let sum = 0;
+    let left = 0;
+    let right = 0;
+    let result = Infinity;
+
+    for (; right < n; right++) {
+        sum += nums[right];
+        while (sum >= target && left <= right) {
+            result = Math.min(result, right - left + 1);
+            sum -= nums[left++];
+        }
+    }
+
+    return result === Infinity ? 0 : result;
+};
