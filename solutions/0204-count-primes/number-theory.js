5bf6b1a1278bc60c335b5f190bed3b57f567c5f4
Khumoyun <contact@xumoyun.uz>
2025-07-13 00:20:54 +0500

    solve: 0204 (js)

---
+++ solutions/0204-count-primes/number-theory.js
@@ -0,0 +1,23 @@
+/*
+ * @title: Number Theory
+ * @time: O(n log log n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const countPrimes = n => {
+    let count = 0;
+    const primes = new Array(n).fill(true);
+    primes[0] = primes[1] = false;
+
+    for (let i = 2; i < n; i++) {
+        if (!primes[i]) continue;
+        count++;
+        for (let j = i * i; j < n; j += i) primes[j] = false;
+    }
+
+    return count;
+};
