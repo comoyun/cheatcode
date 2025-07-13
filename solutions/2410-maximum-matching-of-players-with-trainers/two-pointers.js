af95593682076d98fba3f3ed48ecf8ebda61878d
Khumoyun <contact@xumoyun.uz>
2025-07-13 12:34:23 +0500

    solve: 2410 (js)

---
+++ solutions/2410-maximum-matching-of-players-with-trainers/two-pointers.js
@@ -0,0 +1,34 @@
+/*
+ * @title: Two Pointers
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} players
+ * @param {number[]} trainers
+ * @return {number}
+ */
+const matchPlayersAndTrainers = (players, trainers) => {
+    players.sort((a, b) => a - b);
+    trainers.sort((a, b) => a - b);
+    const n = players.length;
+    const m = trainers.length;
+    let i = 0,
+        j = 0;
+    let result = 0;
+
+    while (i < n && j < m) {
+        const player = players[i];
+        const trainer = trainers[j];
+
+        if (player <= trainer) {
+            i++;
+            result++;
+        }
+
+        j++;
+    }
+
+    return result;
+};
