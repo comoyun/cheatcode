131c92c007cf466a48cd6a7e882435108e26ca6d
Khumoyun <contact@xumoyun.uz>
2025-07-16 14:55:45 +0500

    solve: 3201 (js)

---
+++ solutions/3201-find-the-maximum-length-of-valid-subsequence-i/dynamic-programming.js
@@ -0,0 +1,23 @@
+/*
+ * @title: DFS
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maximumLength = nums => {
+    const dfs = (idx, parity, count = 0) => {
+        if (idx >= nums.length) return count;
+        if (nums[idx] % 2 === parity[count % 2]) count++;
+        return dfs(idx + 1, parity, count);
+    };
+    return Math.max(
+        dfs(0, [0, 0]),
+        dfs(0, [1, 1]),
+        dfs(0, [0, 1]),
+        dfs(0, [1, 0])
+    );
+};
---
+++ solutions/3201-find-the-maximum-length-of-valid-subsequence-i/greedy.js
@@ -0,0 +1,28 @@
+/*
+ * @title: Greedy
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maximumLength = nums => {
+    let result = 0;
+    const patterns = [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+    ];
+    for (const pattern of patterns) {
+        let count = 0;
+
+        for (const num of nums)
+            if (num % 2 === pattern[count % 2]) count++;
+
+        result = Math.max(result, count);
+    }
+    return result;
+};
+
