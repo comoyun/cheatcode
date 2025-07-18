90400c78e1af77032af0628469699beaac7d1d36
Khumoyun <contact@xumoyun.uz>
2025-07-18 14:50:44 +0500

    solve: 2163 (js)

---
+++ solutions/2163-minimum-difference-in-sums-after-removal-of-elements/depth-first-search.js
@@ -0,0 +1,48 @@
+/*
+ * @title: DFS
+ * @time: O(2^n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minimumDifference = nums => {
+    /* WARNING: This gives TLE */
+
+    const n = nums.length;
+    const seqLen = n / 3;
+    let result = Infinity;
+    let len = 0;
+
+    const dfs = idx => {
+        if (len === seqLen) {
+            let leftSum = 0,
+                rightSum = 0,
+                count = 0;
+            for (let i = 0; i < n; i++) {
+                if (nums[i] === null) continue;
+                if (count < seqLen) leftSum += nums[i];
+                else rightSum += nums[i];
+                count++;
+            }
+            result = Math.min(result, leftSum - rightSum);
+            return;
+        }
+        if (idx >= n) return;
+
+        const prev = nums[idx];
+
+        nums[idx] = null;
+        len++;
+        dfs(idx + 1);
+        len--;
+        nums[idx] = prev;
+
+        dfs(idx + 1);
+    };
+    dfs(0);
+
+    return result;
+};
