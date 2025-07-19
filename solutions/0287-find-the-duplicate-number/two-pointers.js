ae6ca359fe355eee8c6db54d657926bca3023a1d
Khumoyun <contact@xumoyun.uz>
2025-07-19 15:07:35 +0500

    solve: 0287 (js)

---
+++ solutions/0287-find-the-duplicate-number/two-pointers.js
@@ -0,0 +1,29 @@
+/*
+ * @title: Two Pointers
+ * @time: O(n)
+ * @space: O(1)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = nums => {
+    let slow = nums[0];
+    let fast = nums[0];
+
+    while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow === fast) break;
+    }
+
+    let ptr1 = nums[0];
+    let ptr2 = slow;
+    while (ptr1 !== ptr2) {
+        ptr1 = nums[ptr1];
+        ptr2 = nums[ptr2];
+    }
+
+    return ptr1;
+};
