c67938d233fd616ad17a63b483b40ff4c739c60e
Khumoyun <contact@xumoyun.uz>
2025-07-19 15:15:14 +0500

    solve: 0287 (js)

---
+++ solutions/0287-find-the-duplicate-number/hash-table.js
@@ -0,0 +1,19 @@
+/*
+ * @title: Hash Table
+ * @time: O(n)
+ * @space: O(n)
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = nums => {
+    const set = new Set();
+    for (const num of nums) {
+        if (set.has(num)) return num;
+        set.add(num);
+    }
+    return -1;
+};
+
