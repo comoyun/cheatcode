6d39e0f34ca91363c3bcbe33764672e48266d622
Khumoyun <contact@xumoyun.uz>
2025-07-29 12:44:24 +0500

    refactor: remove `or` completely

--- solutions/2411-smallest-subarrays-with-maximum-bitwise-or/array-01.js
+++ solutions/2411-smallest-subarrays-with-maximum-bitwise-or/array-01.js
@@ -10,19 +10,16 @@ const smallestSubarrays = nums => {
     const bits = new Array(32).fill(0);
     const result = new Array(n);
 
-    let or = 0;
     let count = 0;
 
     for (let i = n - 1; i >= 0; i--) {
-        or |= nums[i];
         count = 0;
 
         for (let j = 0; j < bits.length; j++)
             if ((nums[i] >> j) & 1) bits[j] = i;
 
         for (let j = 0; j < bits.length; j++)
-            if ((or >> j) & 1) 
-                count = Math.max(count, bits[j] - i + 1);
+            count = Math.max(count, bits[j] - i + 1);
 
         result[i] = count;
     }
