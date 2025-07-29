a924b8e01430100abd531c40cb15981ca7ba676c
Khumoyun <contact@xumoyun.uz>
2025-07-29 12:25:42 +0500

    style: format code

--- solutions/2411-smallest-subarrays-with-maximum-bitwise-or/array-01.js
+++ solutions/2411-smallest-subarrays-with-maximum-bitwise-or/array-01.js
@@ -9,17 +9,20 @@ const smallestSubarrays = nums => {
     const n = nums.length;
     const bits = new Array(32).fill(0);
     const result = new Array(n);
+
     let or = 0;
     let count = 0;
 
     for (let i = n - 1; i >= 0; i--) {
         or |= nums[i];
         count = 1;
+
         for (let j = 0; j < bits.length; j++)
             if ((nums[i] >> j) & 1) bits[j] = i;
 
         for (let j = 0; j < bits.length; j++)
-            if ((or >> j) & 1) count = Math.max(count, bits[j] - i + 1);
+            if ((or >> j) & 1) 
+                count = Math.max(count, bits[j] - i + 1);
 
         result[i] = count;
     }
