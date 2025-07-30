bae2ebfa47c3f3efedf27358c0fa8d1de75cc7ac
Khumoyun <contact@xumoyun.uz>
2025-07-30 18:17:48 +0500

    refactor: simplify code

--- solutions/2419-longest-subarray-with-maximum-bitwise-and/array.js
+++ solutions/2419-longest-subarray-with-maximum-bitwise-and/array.js
@@ -6,15 +6,15 @@
  * @return {number}
  */
 const longestSubarray = nums => {
-    let max = 0;
+    let max = nums.reduce((acc, curr) => Math.max(acc, curr), 0);
     let result = 0;
-    let len = 0;
-    for (const num of nums) max = Math.max(max, num);
+    let streak = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === max) len++;
-        else len = 0;
-        result = Math.max(result, len);
+        if (nums[i] === max) streak++;
+        else streak = 0;
+
+        result = Math.max(result, streak);
     }
     return result;
 };
