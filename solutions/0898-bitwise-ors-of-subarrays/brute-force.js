552d340e68bd58fd1d6e3a0385313cb418c418e9
Khumoyun <contact@xumoyun.uz>
2025-07-31 10:21:14 +0500

    solve: 0898 (js)

---
+++ solutions/0898-bitwise-ors-of-subarrays/brute-force.js
@@ -0,0 +1,22 @@
+// time: O(n ^ 2)
+// space: O(n)
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const subarrayBitwiseORs = arr => {
+    const n = arr.length;
+    const set = new Set();
+
+    for (let i = 0; i < n; i++) {
+        let or = 0;
+        for (let j = i; j < n; j++) {
+            or |= arr[j];
+            set.add(or);
+        }
+    }
+
+    return set.size;
+};
+
