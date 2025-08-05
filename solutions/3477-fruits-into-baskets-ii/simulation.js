c9f7e5a54c6b6783d61503034778b0ae2ab7de22
Khumoyun <contact@xumoyun.uz>
2025-08-05 16:43:23 +0500

    refactor: simplify code

--- solutions/3477-fruits-into-baskets-ii/simulation.js
+++ solutions/3477-fruits-into-baskets-ii/simulation.js
@@ -9,11 +9,10 @@
 const numOfUnplacedFruits = (fruits, baskets) => {
     const n = fruits.length;
     let result = n;
-    for (let i = 0; i < n; i++) {
-        const fruit = fruits[i];
+    for (const fruit of fruits) {
         for (let j = 0; j < n; j++) {
             const basket = baskets[j];
-            if (basket !== null && basket >= fruit) {
+            if (basket && basket >= fruit) {
                 baskets[j] = null;
                 result--;
                 break;
