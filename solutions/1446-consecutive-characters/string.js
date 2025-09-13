56c2ce4288ff8b4c8e97e8abda4aa921483c7b90
Khumoyun <contact@xumoyun.uz>
2025-09-14 01:32:32 +0500

    solve: 1446 (js)

---
+++ solutions/1446-consecutive-characters/string.js
@@ -0,0 +1,19 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maxPower = s => {
+    let count = 1,
+        result = 1;
+
+    for (let i = 1; i < s.length; i++) {
+        count = s[i] === s[i - 1] ? count + 1 : 1;
+        result = Math.max(result, count);
+    }
+
+    return s.length ? result : 0;
+};
+
