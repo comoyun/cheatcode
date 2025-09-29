c1e556aac8287504dbb67224d25c7f6522330f24
Khumoyun <contact@xumoyun.uz>
2025-09-29 23:18:19 +0500

    feat: 0821 (js)

---
+++ solutions/0821-shortest-distance-to-a-character/array.js
@@ -0,0 +1,27 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+const shortestToChar = (s, c) => {
+    const n = s.length;
+    const result = new Array(n);
+    let prev = -Infinity;
+
+    for (let i = 0; i < n; i++) {
+        if (s[i] === c) prev = i;
+        result[i] = i - prev;
+    }
+
+    prev = Infinity;
+    for (let i = n - 1; i >= 0; i--) {
+        if (s[i] === c) prev = i;
+        result[i] = Math.min(result[i], prev - i);
+    }
+
+    return result;
+};
+
