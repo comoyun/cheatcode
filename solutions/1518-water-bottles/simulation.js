06da8577b229e6f78b2b53b9b2f0b728643a5227
Khumoyun <contact@xumoyun.uz>
2025-10-02 00:09:45 +0500

    solve: 1518 (js)

---
+++ solutions/1518-water-bottles/simulation.js
@@ -0,0 +1,22 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+const numWaterBottles = (numBottles, numExchange) => {
+    let result = 0;
+    let full = numBottles;
+    let empty = 0;
+
+    while (full > 0) {
+        result += full;
+        empty += full;
+        full = Math.floor(empty / numExchange); 
+        empty = empty % numExchange; 
+    }
+
+    return result;
+};
