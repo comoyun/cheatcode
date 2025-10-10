0765840dfd6dd68ade596d9d5368ab3663552dba
Khumoyun <contact@xumoyun.uz>
2025-10-10 21:37:15 +0500

    solve: 0419 (js)

---
+++ solutions/0419-battleships-in-a-board/matrix.js
@@ -0,0 +1,19 @@
+// time: O(nm)
+// space: O(1)
+
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+const countBattleships = board => {
+    let count = 0;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] !== 'X') continue;
+            if (i > 0 && board[i - 1][j] === 'X') continue;
+            if (j > 0 && board[i][j - 1] === 'X') continue;
+            count++;
+        }
+    }
+    return count;
+};
