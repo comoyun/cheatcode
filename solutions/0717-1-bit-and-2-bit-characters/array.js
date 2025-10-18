df11b98093e311388ea5430c0a3ebb41b2f6aff8
Khumoyun <contact@xumoyun.uz>
2025-10-18 15:12:06 +0500

    solve: 0717 (js)

---
+++ solutions/0717-1-bit-and-2-bit-characters/array.js
@@ -0,0 +1,13 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+const isOneBitCharacter = bits => {
+    let i = 0;
+    while (i < bits.length - 1) i += bits[i] === 1 ? 2 : 1;
+    return i === bits.length - 1;
+};
+
