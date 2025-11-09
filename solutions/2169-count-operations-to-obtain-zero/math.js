e37ca325e158612aabf19194adf5c33f67a61a40
Khumoyun <contact@xumoyun.uz>
2025-11-09 16:30:04 +0500

    refactor: swap to make `num1` always the biggest

--- solutions/2169-count-operations-to-obtain-zero/math.js
+++ solutions/2169-count-operations-to-obtain-zero/math.js
@@ -10,15 +10,11 @@ const countOperations = (num1, num2) => {
     let result = 0;
 
     while (num1 && num2) {
-        if (num1 >= num2) {
-            const times = Math.floor(num1 / num2);
-            result += times;
-            num1 %= num2;
-        } else {
-            const times = Math.floor(num2 / num1);
-            result += times;
-            num2 %= num1;
-        }
+        const times = Math.floor(num1 / num2);
+        result += times;
+        num1 %= num2;
+
+        [num1, num2] = [num2, num1];
     }
 
     return result;
