bdd2cc26fdd153bb5b03cbb96424622de74401bb
Khumoyun <contact@xumoyun.uz>
2025-11-15 19:36:50 +0500

    solve: 3234 (js)

---
+++ solutions/3234-count-the-number-of-substrings-with-dominant-ones/brute-force.js
@@ -0,0 +1,26 @@
+// time: O(n * n)
+// space: O(1)
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numberOfSubstrings = s => {
+    // this solution gives tle
+    const n = s.length;
+    let result = 0;
+
+    for (let i = 0; i < n; i++) {
+        let ones = 0;
+        let zeros = 0;
+
+        for (let j = i; j < n; j++) {
+            if (s[j] === '1') ones++;
+            else zeros++;
+
+            if (ones >= zeros * zeros) result++;
+        }
+    }
+
+    return result;
+};
