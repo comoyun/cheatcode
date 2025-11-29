9cb995ff79596dc09530161a7187ca15ec365f08
Khumoyun <contact@xumoyun.uz>
2025-11-29 15:44:27 +0500

    solve: 3512 (js)

---
+++ solutions/3512-minimum-operations-to-make-array-sum-divisible-by-k/math.js
@@ -0,0 +1,12 @@
+// time: O(n)
+// space: O(1)
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const minOperations = (nums, k) => {
+    const sum = nums.reduce((prev, curr) => prev + curr, 0);
+    return sum - ((sum / k) | 0) * k;
+};
