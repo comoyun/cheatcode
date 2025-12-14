2a7973947a4dd56135e723aff79b0cf22da2abff
Khumoyun <contact@xumoyun.uz>
2025-12-14 15:54:17 +0500

    solve: 0999 (js)

---
+++ solutions/0999-available-captures-for-rook/simulation.js
@@ -0,0 +1,60 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+const numRookCaptures = board => {
+    let rookRow = -1;
+    let rookCol = -1;
+    let result = 0;
+
+    for (let row = 0; row < board.length; row++) {
+        let found = false;
+        for (let col = 0; col < board[0].length; col++) {
+            if (board[row][col] === 'R') {
+                rookRow = row;
+                rookCol = col;
+                found = true;
+                break;
+            }
+        }
+
+        if (found) break;
+    }
+
+    for (let row = rookRow - 1; row >= 0; row--) {
+        const cell = board[row][rookCol];
+        if (cell === '.') continue;
+        if (cell === 'B') break;
+        result++;
+        break;
+    }
+
+    for (let row = rookRow + 1; row < board.length; row++) {
+        const cell = board[row][rookCol];
+        if (cell === '.') continue;
+        if (cell === 'B') break;
+        result++;
+        break;
+    }
+
+    for (let col = rookCol - 1; col >= 0; col--) {
+        const cell = board[rookRow][col];
+        if (cell === '.') continue;
+        if (cell === 'B') break;
+        result++;
+        break;
+    }
+
+    for (let col = rookCol + 1; col < board[0].length; col++) {
+        const cell = board[rookRow][col];
+        if (cell === '.') continue;
+        if (cell === 'B') break;
+        result++;
+        break;
+    }
+
+    return result;
+};
