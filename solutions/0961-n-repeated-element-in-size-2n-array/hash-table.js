1a4a8f34342e0d2756811b076c2105bf2f7988b7
Khumoyun <contact@xumoyun.uz>
2025-12-14 18:10:12 +0500

    solve: 0961 (js)

---
+++ solutions/0961-n-repeated-element-in-size-2n-array/hash-table.js
@@ -0,0 +1,17 @@
+// time: O(N)
+// space: O(N)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const repeatedNTimes = nums => {
+    const set = new Set();
+
+    for (const num of nums) {
+        if (set.has(num)) return num;
+        set.add(num);
+    }
+
+    return -1;
+};
