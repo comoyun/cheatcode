775e45f40bae57589224b793025c2ac7866a72bb
Khumoyun <contact@xumoyun.uz>
2026-01-07 22:46:54 +0500

    solve: 1339 (js)

---
+++ solutions/1339-maximum-product-of-splitted-binary-tree/depth-first-search.js
@@ -0,0 +1,33 @@
+// time: O(N)
+// space: O(N)
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxProduct = root => {
+    const MOD = 10 ** 9 + 7;
+    let result = 0;
+
+    const getSum = node => {
+        if (!node) return 0;
+        return node.val + getSum(node.right) + getSum(node.left);
+    };
+
+    let sum = getSum(root);
+
+    const dfs = node => {
+        if (!node) return 0;
+
+        let leftSum = dfs(node.left);
+        let rightSum = dfs(node.right);
+        const cur = node.val + leftSum + rightSum;
+
+        result = Math.max(result, cur * (sum - cur));
+        return cur;
+    };
+
+    dfs(root);
+    return result % MOD;
+};
+
