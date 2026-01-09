6b809e557e07806419c9650fbb84dc0a6db88048
Khumoyun <contact@xumoyun.uz>
2026-01-09 11:51:26 +0500

    solve: 0865 (js)

---
+++ solutions/0865-smallest-subtree-with-all-the-deepest-nodes/depth-first-search.js
@@ -0,0 +1,34 @@
+// time: O(N)
+// space: O(H)
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const subtreeWithAllDeepest = root => {
+    const getMaxDepth = node => {
+        if (!node) return 0;
+        return 1 + Math.max(getMaxDepth(node.left), getMaxDepth(node.right));
+    };
+
+    const maxDepth = getMaxDepth(root);
+    let result = null;
+
+    const dfs = (node, depth = 1) => {
+        if (!node) return null;
+        if (depth === maxDepth) {
+            result = node;
+            return true;
+        }
+
+        const left = dfs(node.left, depth + 1);
+        const right = dfs(node.right, depth + 1);
+
+        if (left && right) result = node;
+        return left || right;
+    };
+
+    dfs(root);
+    return result;
+};
+
