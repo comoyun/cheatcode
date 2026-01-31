9154d68c987d57861a887b924b03dd453918bcfb
Khumoyun <contact@xumoyun.uz>
2026-01-31 07:03:35 +0500

    solve: 0744 (js)

---
+++ solutions/0744-find-smallest-letter-greater-than-target/sorting.js
@@ -0,0 +1,19 @@
+// time: O(N)
+// space: O(N)
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+const nextGreatestLetter = (letters, target) => {
+    letters = letters.sort();
+    for (const letter of letters) if (letter > target) return letter;
+    return letters[0];
+};
+
+// Examples:
+console.log(nextGreatestLetter(['c', 'f', 'j'], 'a'));
+console.log(nextGreatestLetter(['c', 'f', 'j'], 'c'));
+console.log(nextGreatestLetter(['x', 'x', 'y', 'y'], 'z'));
+
