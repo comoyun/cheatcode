a8675db843368e7f563208e466556a4b680e90ce
Khumoyun <contact@xumoyun.uz>
2026-04-19 14:08:33 +0500

    solve: 1855 (js)

---
+++ solutions/1855-maximum-distance-between-a-pair-of-values/two-pointers.js
@@ -0,0 +1,22 @@
+// time: O(n)
+// space: O(1)
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const maxDistance = (nums1, nums2) => {
+    let i = 0,
+        j = 0,
+        result = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] > nums2[j]) i++;
+        else if (i > j) j++;
+        else result = Math.max(result, j++ - i);
+    }
+
+    return result;
+};
+
