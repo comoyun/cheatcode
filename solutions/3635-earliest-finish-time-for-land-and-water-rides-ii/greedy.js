1611d6424ef66405eb887eb3f8a50be800f3442c
Khumoyun <contact@xumoyun.uz>
2026-06-04 00:00:47 +0500

    solve: 3635 (js)

---
+++ solutions/3635-earliest-finish-time-for-land-and-water-rides-ii/greedy.js
@@ -0,0 +1,43 @@
+// time: O(n)
+// space: O(1)
+
+/**
+ * @param {number[]} landStartTime
+ * @param {number[]} landDuration
+ * @param {number[]} waterStartTime
+ * @param {number[]} waterDuration
+ * @return {number}
+ */
+const earliestFinishTime = (
+    landStartTime,
+    landDuration,
+    waterStartTime,
+    waterDuration
+) => {
+    const earliest = (start, duration, offset) => {
+        const n = start.length;
+        let result = Infinity;
+
+        for (let i = 0; i < n; i++) {
+            const s = Math.max(start[i], offset);
+            const d = duration[i];
+            result = Math.min(result, s + d);
+        }
+
+        return result;
+    };
+
+    return Math.min(
+        earliest(
+            waterStartTime,
+            waterDuration,
+            earliest(landStartTime, landDuration, 0)
+        ),
+        earliest(
+            landStartTime,
+            landDuration,
+            earliest(waterStartTime, waterDuration, 0)
+        )
+    );
+};
+
