30c0ffc69cec23c3fc564a18156a79e7c6b96f59
Khumoyun <contact@xumoyun.uz>
2026-08-16 13:28:56 +0500

    refactor: better use static array

--- solutions/2029-stone-game-ix/greedy.cpp
+++ solutions/2029-stone-game-ix/greedy.cpp
@@ -5,10 +5,11 @@ using namespace std;
 class Solution {
   public:
     bool stoneGameIX(vector<int> stones) {
-        vector<int> mod = {0, 0, 0};
-        for (const int num : stones) mod[num % 3]++;
+        int mod[3] = {};
 
+        for (const int num : stones) mod[num % 3]++;
         if (mod[0] % 2 == 0) return mod[1] > 0 && mod[2] > 0;
+
         return abs(mod[1] - mod[2]) > 2;
     }
 };
