c48ade3fa13662849c6f1907774f656d86e653ee
Khumoyun <contact@xumoyun.uz>
2026-08-23 14:44:43 +0500

    fix: use class and method

--- solutions/3622-check-divisibility-by-digit-sum-and-product/math.cpp
+++ solutions/3622-check-divisibility-by-digit-sum-and-product/math.cpp
@@ -1,21 +1,17 @@
-#include <iostream>
-#include <string>
+class Solution {
+  public:
+    bool checkDivisibility(int n) {
+        int s = 0;
+        int p = 1;
+        const int num = n;
 
-using namespace std;
+        while (n) {
+            const int digit = n % 10;
+            p *= digit;
+            s += digit;
+            n /= 10;
+        }
 
-int main() {
-    int n;
-    int s = 0;
-    int p = 1;
-    cin >> n;
-    const int num = n;
-
-    while (n) {
-        const int digit = n % 10;
-        p *= digit;
-        s += digit;
-        n /= 10;
+        return num % (p + s) == 0;
     }
-
-    cout << (num % (p + s) == 0 ? "Bo'linadi" : "Bo'linmaydi") << endl;
-}
+};
