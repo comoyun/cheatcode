e601bc96de97c27f983689ccc7a75f36157d29d9
Khumoyun <contact@xumoyun.uz>
2026-09-04 14:39:23 +0500

    solve: 3903 (js,cpp)

---
+++ solutions/3903-smallest-stable-index-i/brute-force.cpp
@@ -0,0 +1,26 @@
+// time: O(n)
+// space: O(n)
+
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int firstStableIndex(vector<int> &nums, int k) {
+        const int n = nums.size();
+        int mx = 0;
+
+        for (int i = 0; i < n; i++) {
+            mx = max(mx, nums[i]);
+            int mn = nums[i];
+            for (int j = i; j < n; j++) mn = min(mn, nums[j]);
+
+            if (mx - mn <= k) return i;
+        }
+
+        return -1;
+    }
+};
---
+++ solutions/3903-smallest-stable-index-i/brute-force.js
@@ -0,0 +1,21 @@
+// time: O(n)
+// space: O(n)
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const firstStableIndex = (nums, k) => {
+    const n = nums.length;
+    let max = 0;
+
+    for (let i = 0; i < n; i++) {
+        let min = nums[i];
+        max = Math.max(max, nums[i]);
+        for (let j = i; j < n; j++) min = Math.min(min, nums[j]);
+        if (max - min <= k) return i;
+    }
+
+    return -1;
+};
