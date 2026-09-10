ed85896def48a02d2a0719f478123f8e1872e4e9
Khumoyun <contact@xumoyun.uz>
2026-09-10 11:13:09 +0500

    solve: 2265 (js)

---
+++ solutions/2265-count-nodes-equal-to-average-of-subtree/depth-first-search.js
@@ -0,0 +1,27 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const averageOfSubtree = root => {
+    const dfs = node => {
+        if (!node) return { sum: 0, nodes: 0, result: 0 };
+
+        const [left, right] = [dfs(node.left), dfs(node.right)];
+
+        const total = left.sum + right.sum + node.val;
+        const nodes = left.nodes + right.nodes + 1;
+        const average = Math.floor(total / nodes);
+
+        let result = left.result + right.result;
+
+        if (node.val === average) result++;
+
+        return { sum: total, nodes, result };
+    };
+
+    return dfs(root).result;
+};
+
