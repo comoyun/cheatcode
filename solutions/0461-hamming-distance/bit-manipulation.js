cb2ce697a3787f4d8bcb29d9dc13be5ffeee59f2
Khumoyun <contact@xumoyun.uz>
2026-09-13 11:39:18 +0500

    solve: 0461 (js)

---
+++ solutions/0461-hamming-distance/bit-manipulation.js
@@ -0,0 +1,23 @@
+// time: O()
+// space: O()
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const hammingDistance = (x, y) => {
+    let a = x;
+    let b = y;
+    let result = 0;
+
+    while (a || b) {
+        const bitA = a & 1;
+        const bitB = b & 1;
+        result += bitA ^ bitB;
+        a >>= 1;
+        b >>= 1;
+    }
+
+    return result;
+};
